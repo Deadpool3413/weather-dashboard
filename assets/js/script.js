e4bb3b3f061a6cd32d3a8e8e80c156ffef5f98ab
Jaime Rivas <j.rivas4818@gmail.com>
2022-03-18 21:14:16 -0700

    creating html layout

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const API_KEY = "efce923fdeaa8b3017a1d2da22a5ca95"
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+const defaultCoords = { lat: 33.625274, lon: -112.218690 };
+
+$(document).ready(() => {
+    // Initial data fetch
+    fetchWeather();
+})
+
+// easy way to reuse and build the url
+/**
+ * 
+ * @param lat: latitude desired - default from defaultCoords
+ * @param long: longitude desired - default from defaultCoords
+ * @returns Promise<whatever the data is>
+ */
+const getUrl = (lat, lon) => {
+    const urlLat = lat || defaultCoords.lat;
+    const urlLon = lon || defaultCoords.lon
+    return `${BASE_URL}?units=imperial&lat=${urlLat}&lon=${urlLon}&appid=${API_KEY}`;
+}
+
+const fetchWeather = async (lat, lon) => {
+
+    return await fetch(getUrl())
+        .then((res) => res.json())
+        .then((res) => $('.temp').append(res.current.temp))
+        .catch((err) => state['error'] = err);
+};
